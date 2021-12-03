72aedc9c2263ee3828f65f62f2422031990f1d77
McRaymond <82787775+McRaymond@users.noreply.github.com>
2021-12-03 13:47:31 -0500

    Added new files

---
+++ main.js
@@ -0,0 +1,84 @@
+function onmouseoverhomebtn() {
+    const b = document.querySelector("#button-box");
+    b.style.display = "inherit";
+    b.style.left = "58.6vw";
+    b.style.width = "100px";
+    b.style.height = "45px";
+    
+}
+function onmouseexithomebtn() {
+    const b = document.querySelector("#button-box");
+
+    b.style.display = "none";
+}
+
+function onmouseoveraboutbtn() {
+    const b = document.querySelector("#button-box");
+    b.style.display = "inherit";
+    b.style.left = "68.5vw";
+    b.style.width = "110px";
+    b.style.height = "45px";
+    
+}
+function onmouseexitwebbtn() {
+    const b = document.querySelector("#button-box");
+
+    b.style.display = "none";
+}
+
+function onmouseoverservicetbtn() {
+    const b = document.querySelector("#button-box");
+    b.style.display = "inherit";
+    b.style.left = "78.9vw";
+    b.style.width = "120px";
+    b.style.height = "45px";
+    
+}
+function onmouseexitcontactbtn() {
+    const b = document.querySelector("#button-box");
+
+    b.style.display = "none";
+}
+
+function onmouseovercontactbtn() {
+    const b = document.querySelector("#button-box");
+    b.style.display = "inherit";
+    b.style.left = "89.2vw";
+    b.style.width = "120px";
+    b.style.height = "45px";
+    
+}
+
+function onmouseexitcontactbtn() {
+    const b = document.querySelector("#button-box");
+
+    b.style.display = "none";
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("closebtn").style.display = "inherit";
+    document.getElementById("homebtn-side").style.display = "inherit";
+    document.getElementById("web-btn-side").style.display = "inherit";
+    document.getElementById("contact-btn-side").style.display = "inherit";
+    document.getElementById("about-btn-side").style.display = "inherit";
+    document.getElementById("side-bar-logo").style.display = "inherit";
+}
+  
+  /* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.querySelector("#sideNav").style.width = "0";
+    document.querySelector("#closebtn").style.display = "none";
+    document.querySelector("#homebtn-side").style.display = "none";
+    document.querySelector("#web-btn-side").style.display = "none";
+    document.querySelector("#contact-btn-side").style.display = "none";
+    document.querySelector("#about-btn-side").style.display = "none";
+    document.querySelector("#side-bar-logo").style.display = "none";
+}
+
+
+
+/*side-bar-box/selector*/
+
+
